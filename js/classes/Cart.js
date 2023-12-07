f227a1141024db3ba4c74f0521d086f81b6b2a3f
Alexandra Masi <masialexandra@gmail.com>
2023-12-07 09:37:04 +0100

    Ajout d'explications

--- js/classes/Cart.js
+++ js/classes/Cart.js
@@ -4,6 +4,24 @@ class Cart {
         this.shipment = new Shipment();
     }
 
+/*Explications détaillées updateTotals() :
+
+Calcul des totaux :
+totalQty : Utilise la méthode calculateTotalArticles de la classe Lines pour obtenir la quantité totale d'articles dans le panier.
+totalArticlesPrice : Utilise la méthode calculateTotalArticlesPrice de la classe Lines pour obtenir le prix total des articles dans le panier.
+totalPrice : Additionne le prix total des articles avec le coût de l'expédition (this.shipment.price) pour obtenir le prix total général du panier.
+
+Mise à jour des éléments HTML :
+Utilise document.getElementById pour obtenir des références aux éléments HTML que vous souhaitez mettre à jour.
+Utilise la propriété textContent pour mettre à jour le contenu textuel de ces éléments avec les nouveaux totaux calculés.
+
+Mise à jour de l'interface utilisateur :
+Appelle la méthode updateCartUI pour mettre à jour l'interface utilisateur avec les nouvelles données du panier.
+
+Affichage des totaux dans la console :
+Utilise console.log pour afficher les totaux calculés dans la console du navigateur. Utile pour le débogage.
+*/ 
+    
     updateTotals() {
         let totalQty = this.lines.calculateTotalArticles();
         let totalArticlesPrice = this.lines.calculateTotalArticlesPrice();
@@ -96,13 +114,24 @@ document.querySelectorAll(".item-input").forEach((input) => {
     });
 
     // Ajout d'un écouteur de clic à chaque bouton de suppression de ligne
+
+    /*Explications détaillées :
+Écouteur d'événements (addEventListener) :
+tableBody.addEventListener('click', function(event) { : Ajoute un écouteur d'événements au <tbody> de la table. Cela signifie que l'événement est déclenché lorsqu'un clic se produit à l'intérieur du corps de la table.
+
+Vérification de la classe (classList.contains) :
+if (event.target.classList.contains('remove-row')) { : Vérifie si l'élément sur lequel l'utilisateur a cliqué a la classe CSS 'remove-row'. Cela permet de déterminer si l'événement de clic a été déclenché par le bouton "X" (bouton de suppression) dans une ligne de la table.
+
+Obtention de l'indice de la ligne (indexOf) :
+const index = [...tableBody.children].indexOf(event.target.closest('tr')); : Obtient l'indice de la ligne (<tr>) qui contient le bouton sur lequel l'utilisateur a cliqué. Cela se fait en utilisant event.target.closest('tr') pour remonter dans l'arborescence DOM à partir du bouton cliqué jusqu'à trouver le parent le plus proche de type <tr>. Ensuite, indexOf est utilisé pour trouver l'indice de cette ligne dans la liste des enfants du <tbody>.
+
+Suppression de la ligne du panier (cart.lines.removeLine(index)) :
+cart.lines.removeLine(index); : Appelle la méthode removeLine de l'objet lines de la classe Cart pour supprimer la ligne correspondante du panier.
+
+Mise à jour des totaux (cart.updateTotals()) :
+cart.updateTotals(); : Après avoir supprimé la ligne, mise à jour des totaux du panier en appelant la méthode updateTotals de la classe Cart. Cela garantit que les totaux reflètent les changements dans le panier après la suppression de la ligne.*/
+
     tableBody.addEventListener('click', function(event) {
-        /*if (event.target.classList.contains('remove-row')) {
-            const row = event.target.closest('tr');
-            const itemInput = row.querySelector('.item-input');
-            cart.lines.removeLine(itemInput);
-            cart.updateTotals();
-        }*/
         if (event.target.classList.contains('remove-row')) {
             const index = [...tableBody.children].indexOf(event.target.closest('tr'));
             cart.lines.removeLine(index);
