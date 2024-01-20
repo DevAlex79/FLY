8171fc02df9299aba095bdf08fa0142dd2b10be3
Alexandra Masi <masialexandra@gmail.com>
2024-01-20 15:58:31 +0100

    Création articles.json + articles.js + modif articles.html

---
+++ js/articles.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch les données des articles
+    fetch('articles.json')
+        .then(response => response.json())
+        .then(articles => {
+        const articlesContainer = document.getElementById('articles');
+
+        // Afficher chaque article dans la liste
+        articles.forEach(article => {
+            const articleElement = document.createElement('article');
+            articleElement.innerHTML = `
+                <div class="image">
+                    <a href="singlearticle.html?id=${article.id}" target="_blank">
+                    <img src="${article.image}" alt="${article.title}">
+                    </a>
+                <div class="texte-article">
+                    <a href="singlearticle.html?id=${article.id}" target="_blank">
+                    <h2 class="categorie-${article.category.toLowerCase()}">${article.category}</h2>
+                    </a>
+                <h1 class="titre">${article.title}</h1>
+                <p>${article.description}</p>
+                </div>
+                </div>
+            `;
+            articlesContainer.appendChild(articleElement);
+        });
+    })
+        .catch(error => console.error('Erreur lors de la récupération des données des articles:', error));
+});
